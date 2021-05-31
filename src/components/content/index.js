fc01499fc8334c2a1b91a5843a8cc741b42bc523
DevLeoRamos <lmassalairamos@gmail.com>
2021-05-31 16:59:55 -0300

    vez Leonardo

--- src/components/content/index.js
+++ src/components/content/index.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import { Grid } from '@material-ui/core';
 
 import { StyledContainer } from './styles';
 
 
 const Content = ({ }) => (
   <StyledContainer maxWidth={'lg'}>
-
+    <Grid container spacing={1}>
+      <Grid container item xs={12} spacing={3}>
+        a
+      </Grid>
+      <Grid container item xs={12} spacing={3}>
+        b
+      </Grid>
+      <Grid container item xs={12} spacing={3}>
+        c
+      </Grid>
+    </Grid>
   </StyledContainer>
 );
 
