1cc74ccc0f86c1a0b8da051c7ec060239d41c828
ArthurBiasibetti <arthurbiasibettifarias@gmail.com>
2021-05-31 17:14:35 -0300

    vez do arthur :D :bug:

--- src/components/content/index.js
+++ src/components/content/index.js
@@ -1,20 +1,21 @@
 import React from 'react';
 import { Grid } from '@material-ui/core';
 
-import { StyledContainer } from './styles';
+import { StyledContainer, Title, Price } from './styles';
 
 
 const Content = ({ }) => (
   <StyledContainer maxWidth={'lg'}>
-    <Grid container spacing={1}>
-      <Grid container item xs={12} spacing={3}>
-        a
+     <Grid container spacing={1}>
+      <Grid container item xs={12} lg={4} spacing={3}>
+          <img src="https://images7.kabum.com.br/produtos/fotos/85197/85197_index_g.jpg" alt="ssd 128GB"  style={{width:'128px', height: '128px'}}/>
       </Grid>
-      <Grid container item xs={12} spacing={3}>
-        b
+      <Grid container item xs={12} lg={4} spacing={3}>
+        <Title>Disco sólido interno</Title>
+        <Price>R$ 258</Price>
       </Grid>
-      <Grid container item xs={12} spacing={3}>
-        c
+      <Grid container item xs={12} lg={4} spacing={3}>
+        teste
       </Grid>
     </Grid>
   </StyledContainer>
--- src/components/content/styles.js
+++ src/components/content/styles.js
@@ -1,11 +1,21 @@
-import Styled from 'styled-components';
+import styled from 'styled-components';
 
 import { Container } from '@material-ui/core';
 
-const StyledContainer = Styled(Container)`
+const StyledContainer = styled(Container)`
   background-color: ${({ theme }) => theme.colors.white};
   padding: 20px;
   margin-top: 35px;
 `;
 
-export { StyledContainer }
+const Title = styled.h1`
+  font-size: 22px;
+`;
+
+
+const Price = styled.h1`
+  font-size: 36px;
+  font-weight: bold;
+`;
+
+export { StyledContainer, Title, Price }
